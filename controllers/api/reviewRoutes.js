fc9a32e853ea69ac8c396817918ae82e347614a8
Holy Paul <brijeeta@gmail.com>
2021-11-08 20:56:10 -0500

    added delete functionality

--- controllers/api/reviewRoutes.js
+++ controllers/api/reviewRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { Blogs } = require('../../models');
+const withAuth = require('../../utils/auth');
 
 
 // // GET route for single country search
@@ -24,6 +25,24 @@ const { Blogs } = require('../../models');
 //     }
 // })
 
+// // GET route for single country search
+router.get('/', async(req, res) => {
+
+    try {
+        const blogs = await Blogs.findAll({})
+
+        if (!blogs) {
+            res.status(404).json({ message: 'No blogs for this country!' })
+            return
+        }
+
+        res.status(200).json(blogs)
+
+    } catch (err) {
+        res.status(400).json(err)
+    }
+})
+
 // Post new blog post
 router.post('/', async(req, res) => {
     try {
@@ -38,22 +57,42 @@ router.post('/', async(req, res) => {
 });
 
 
-// // Delete blogpost with specific post id
-// router.delete('/:id', async(req, res) => {
+// Delete blogpost with specific post id
+router.delete('/:id', async(req, res) => {
+    try {
+        const deleteBlog = await Blogs.destroy({
+            where: {
+                id: req.params.id,
+
+            },
+        });
+        if (!deleteBlog) {
+            res.status(404).json({ message: 'No blog found with this id!' });
+            return;
+        }
+        res.status(200).json(deleteBlog);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// router.delete('/:id', withAuth, async(req, res) => {
 //     try {
-//         const deleteBlog = await Blogs.destroy({
+//         const projectData = await Project.destroy({
 //             where: {
 //                 id: req.params.id,
+//                 user_id: req.session.user_id,
 //             },
 //         });
-//         if (!deleteBlog) {
-//             res.status(404).json({ message: 'No blog found with this id!' });
+
+//         if (!projectData) {
+//             res.status(404).json({ message: 'No project found with this id!' });
 //             return;
 //         }
-//         res.status(200).json(deleteBlog);
+
+//         res.status(200).json(projectData);
 //     } catch (err) {
 //         res.status(500).json(err);
 //     }
 // });
-
 module.exports = router;
